7706ac3bce665b6f7deba0f13e7c800dc77f4a84
Abhi Yerra <abhi@berkeley.edu>
2022-01-13 14:43:31 -0800

    Added code and readme

---
+++ main.tf
@@ -0,0 +1,27 @@
+data "http" "ipv4" {
+  url = "https://www.cloudflare.com/ips-v4"
+}
+
+data "http" "ipv6" {
+  url = "https://www.cloudflare.com/ips-v6"
+}
+
+resource "aws_security_group" "cloudflare" {
+  name        = "cloudflare-ingress"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = aws_vpc.main.id
+
+  tags = {
+    Name = "cloudflare-ingress"
+  }
+}
+
+resource "aws_security_group_rule" "cloudflare" {
+  type              = "ingress"
+  from_port         = 0
+  to_port           = 65535
+  protocol          = "tcp"
+  cidr_blocks       = split("\n", data.http.ipv4.body)
+  ipv6_cidr_blocks  = split("\n", data.http.ipv6.body)
+  security_group_id = resource.aws_security_group.cloudflare.id
+}
